7ba732028d82c8c1ec2bf282748ac91469b27f45
sbigstar0310 <sbigstar0930@gmail.com>
2023-09-29 14:54:59 +0900

    add functions [getDiffTime&calculAlchol&printTime]

--- alchol-web/scripts/script.js
+++ alchol-web/scripts/script.js
@@ -8,7 +8,7 @@ P : 사람 체중
 R : 성별계수 (남: 0.7, 여: 0.6)
 
 > 혈중 알콜 농도는 음주 후 90분까지 최대치.
-> 혈중 알콜 농도는 최대치 이후 시간 당 0.015% 감소.
+> 혈중 알콜 농도는 최대치 이후 시간 당 0.008% 감소.
 
 맥주: 355ml, 500ml 4.50%
 소주: 360ml, 14%
@@ -29,9 +29,47 @@ function printResult() {
   var beer = document.getElementById("맥주").value;
   var soju = document.getElementById("소주").value;
   var makgeolli = document.getElementById("막걸리").value;
+  var drinkTime = document.getElementById("drinkTime").value;
+
+  var diffTime = getDiffTime(drinkTime);
+
   var A =
     (beer * 355 * 0.045 + soju * 360 * 0.14 + makgeolli * 750 * 0.06) * 0.7984;
   var C = (0.7 * A) / (10 * P * R);
+  console.log("알콜 농도는: " + C);
+  resultTime = calculAlchol(diffTime, C);
+  printTime(resultTime);
+}
 
-  alert("혈중 알콜 농도는: " + C);
+function getDiffTime(drinkTime) {
+  var timeNowDate = new Date();
+  var timeDrinkDate = new Date(drinkTime);
+  var diffTime = Math.round((timeNowDate - timeDrinkDate) / (1000 * 60));
+  console.log("시간 차이는: " + diffTime);
+  return diffTime;
+}
+
+function calculAlchol(diffTime, alchol) {
+  var result;
+  if (diffTime <= 90) {
+    result = (60 / 0.008) * (alchol - 0.05);
+  } else {
+    result = (60 / 0.008) * (alchol - 0.05 - (0.008 / 60) * (diffTime - 90));
+  }
+  console.log("운전 가능 시간은: " + Math.ceil(result));
+  return Math.ceil(result);
+}
+
+function printTime(resultTime) {
+  if (resultTime <= 0) {
+    alert("지금 운전 가능합니다.");
+    return;
+  }
+  alert(
+    "운전 가능 시간은 " +
+      Math.floor(resultTime / 60) +
+      "시간 " +
+      (resultTime % 60) +
+      "분 이후입니다."
+  );
 }
